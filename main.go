ff6f6416cb9d2bb5290ddeaf9f0da66d7753be58
Rohan Cribbs <rohan.cribbs@thoughtforger.com>
2018-11-24 13:53:53 -0800

    Fix some backwards logic with https setting and add a port setting.

--- main.go
+++ main.go
@@ -9,18 +9,26 @@ import (
 )
 
 var httpsEnvVar = "HEALTHCHECK_HTTPS"
+var portEnvVar = "HEALTHCHECK_PORT"
 
 func main() {
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{
 		InsecureSkipVerify: true,
 	}
+	port, _ := os.LookupEnv(httpsEnvVar)
+	if len(port) > 0 {
+		port = fmt.Sprintf(":%s", port)
+	}
 	httpsEnv, found := os.LookupEnv(httpsEnvVar)
 	https := ""
-	if !found || len(httpsEnv) == 0 || strings.ToUpper(httpsEnv)[0] != 't' {
+	if found && len(httpsEnv) != 0 && strings.ToUpper(httpsEnv)[0] == 't' {
 		https = "s"
 	}
-	resp, err := http.Get(fmt.Sprintf("http%s://127.0.0.1/health", https))
+	resp, err := http.Get(fmt.Sprintf(
+		"http%s://127.0.0.1%s/health", https, port,
+	))
 	if err != nil {
+		fmt.Println("Error while connecting:", err)
 		os.Exit(1)
 	}
 	if resp.StatusCode != 200 {
